625e11d2fea9ab5be9cfd29bceeeabbfdaa8ab7b
Imaduddin Haetami <deen24id@gmail.com>
2025-06-16 19:33:14 +0700

    `app/members` Add `button-refresh` to the page.

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -12,6 +12,7 @@ import { member } from "@/db/schema";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { TableCellViewer } from "./component";
 import { eq, desc } from "drizzle-orm";
+import { ButtonRefresh } from "@/components/button-refresh";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
@@ -26,7 +27,8 @@ export default async function Page() {
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <div className="flex justify-end px-4 lg:px-6">
+          <div className="flex justify-end px-4 lg:px-6 gap-2">
+            <ButtonRefresh />
             <TableCellViewer
               variant="create"
               name=""
